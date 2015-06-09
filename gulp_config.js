103757cce433c47a9ba5654e7323565678c70c6b
Lauren Byrwa <byrwa@palantir.net>
2015-06-09 11:56:37 -0500

    fed-init

---
+++ gulp_config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.browsersync_mode = "browser-sync-prototype-only";
+config.compile_mode = "prototype-only-sass";
+config.speedtest_mode = "prototype-only-sitespeed";
+
+module.exports = config;
