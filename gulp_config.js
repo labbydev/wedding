16d18bdf657bfc5e4215d5cea884a36fe4b89bf7
Lauren Byrwa <byrwa@palantir.net>
2015-06-11 09:30:54 -0500

    Update 2015-06-11T14:30:54.202Z

--- gulp_config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.browsersync_mode = "browser-sync-prototype-only";
-config.compile_mode = "prototype-only-sass";
-config.speedtest_mode = "prototype-only-sitespeed";
-
-module.exports = config;
