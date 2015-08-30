3dbcf451f06703262e2a7e46fe001da869318d05
Lauren Byrwa <labyrwa@gmail.com>
2015-08-30 15:19:38 -0500

    Responsive header and main menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,25 @@ setInterval(function () {
   countdown.innerHTML = '<div class="center"><div class="countdown__period"><div class="countdown__period-time">' + days + '</div><div class="countdown__period-label"> Days</div></div><div class="countdown__period"><div class="countdown__period-time">' + hours + '</div><div class="countdown__period-label"> Hours</div></div><div class="countdown__period"><div class="countdown__period-time">' + minutes + '</div><div class="countdown__period-label"> Minutes</div></div><div class="countdown__period"><div class="countdown__period-time">' + seconds + '</div><div class="countdown__period-label"> Seconds</div></div></div>';
 
 }, 1000);
+
+;( function( $ ) {
+  "use strict";
+
+  // on scroll add fixed class to the header
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+      $('.site__header').addClass("is-fixed");
+    }
+    else{
+      $('.site__header').removeClass("is-fixed");
+    }
+  });
+
+  // open and close mobile menu toggle
+  $('.toggle--menu').click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('is-open');
+    $(this).parent().toggleClass('is-open');
+  });
+
+} ) ( jQuery );
